bbf424a8e4eb462b0b5ab3c4b9693c405fe7a02c
Melinapgt <poignetmelina@gmail.com>
2022-03-02 17:06:53 +0100

    corrections

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -9,40 +9,13 @@ const GOOGLE_API_KEY = process.env.MY_GOOGLE_API_KEY;
 
 router.get("/restaurant", async (req, res) => {
   try {
-    console.log(req.query);
+    console.log("req.query==>", req.query);
     const { placeId } = req.query;
     // console.log(placeId);
 
     const restaurant = await Restaurant.findOne({ placeId: placeId });
 
-    //googlemap--------------
-
-    const latitude = restaurant.location.lat;
-    const longitude = restaurant.location.lng;
-    console.log(latitude, longitude);
-    //ajout de la map
-    const initMap = () => {
-      //localisation du restaurant
-      const restaurantLocation = { lat: latitude, lng: longitude };
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: restaurantLocation,
-      });
-      //Marker
-      const marker = new google.maps.Marker({
-        position: restaurantLocation,
-        map: map,
-      });
-    };
-
-    const map = async () => {
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&callback=${initMap}&libraries=&v=weekly`
-      );
-    };
-
-    const getMap = map();
-    res.status(200).json({ restaurant: restaurant, map: getMap });
+    res.status(200).json(restaurant);
   } catch (error) {
     console.log("error.response ==>", error.response);
     res.status(400).json({ error: "Bad request" });
