505c7579f8cdda49c8a6e6ae4bd9851f0d001116
Melinapgt <poignetmelina@gmail.com>
2022-03-04 12:16:16 +0100

    corrections

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -9,14 +9,32 @@ router.get("/restaurant", async (req, res) => {
     console.log("req.query==>", req.query);
     const { placeId } = req.query;
     // console.log(placeId);
+    // console.log(req.query.placeId);
 
-    const restaurant = await Restaurant.findOne({ placeId: placeId });
+    const restaurant = await Restaurant.findOne({ placeId: Number(placeId) });
+    console.log(Object.keys(restaurant._doc));
 
+    console.log(
+      "restaurant.nearbyPlacesIds==>",
+      restaurant._doc.nearbyPlacesIds
+    );
     // requête nearby
+    const nearbyRestaurants = [];
 
-    res.status(200).json(restaurant);
+    for (const placeId of restaurant._doc.nearbyPlacesIds) {
+      console.log("nearby placeId", placeId);
+      const nearby = await Restaurant.findOne({ placeId });
+      console.log(nearby);
+      nearbyRestaurants.push(nearby);
+    }
+
+    console.log(nearbyRestaurants);
+
+    res
+      .status(200)
+      .json({ restaurant: restaurant, nearbyRestaurants: nearbyRestaurants });
   } catch (error) {
-    console.log("error.response ==>", error.response);
+    console.log("error.response ==>", error.message);
     res.status(400).json({ error: "Bad request" });
   }
 });
