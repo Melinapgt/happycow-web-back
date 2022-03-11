03ee5ef2265e5347b3efaed3ca3785932e0cdad8
Melinapgt <poignetmelina@gmail.com>
2022-03-11 11:48:45 +0100

    console log added

--- routes/getUserAccount.js
+++ routes/getUserAccount.js
@@ -8,8 +8,10 @@ router.get("/my-account", async (req, res) => {
   try {
     console.log(req.query);
     const { username } = req.query;
+    console.log(username);
 
     const userAccount = await User.findOne({ username });
+    console.log(userAccount);
 
     res.status(200).json(userAccount);
   } catch (error) {
